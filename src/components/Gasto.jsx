330e8422c3054c021026e59556b8d2fa04d4383a
nicolas britto <nicobritto21@gmail.com>
2023-07-02 12:20:31 -0300

    Gastos Imagenes dinamicas

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { formatiarFecha } from '../helpers'; 
+import iconoAhorro from '../img/icono_ahorro.svg';
+import iconoCasa from '../img/icono_casa.svg';
+import iconoComida from '../img/icono_comida.svg';
+import iconoGastos from '../img/icono_gastos.svg';
+import iconoOcio from '../img/icono_ocio.svg';
+import iconoSalud from '../img/icono_salud.svg';
+import iconoSuscripciones from '../img/icono_suscripciones.svg';
 
-const Gasto = () => {
+const diccionarioIconos ={
+
+  ahorro : iconoAhorro,
+  comida :iconoComida,
+  casa :iconoCasa,
+  gastos :iconoGastos,
+  ocio :iconoOcio,
+  salud :iconoSalud,
+  suscripciones :iconoSuscripciones
+
+}
+
+const Gasto = ({gasto}) => {
+
+const {categoria,nombre,cantidad,id,fecha}=gasto;
+
   return (
-    <div>
-        <p>Desde gastos jsx</p>
+    <div className='gasto sombra'>
+        <div className='contenido-gasto'>
+          <img 
+            src={diccionarioIconos[categoria]}
+            alt='Icono Gastos'
+          />
+            <div className='descripcion-gasto'>
+              <p className='categoria'> {categoria} </p>
+              <p className='nombre-gasto'> {nombre} </p>
+              <p className='fecha-gasto'>Agregado el:{''} <span>{formatiarFecha(fecha)}</span> </p>
+            </div>
+           </div>
+           <p className='cantidad-gasto'>${cantidad}</p>
     </div>
   )
 }
